84b6205701ed6c5f8b808ab985d70c0bd8636eb0
Abby Nacional <aan16@duke.edu>
2022-01-01 16:40:13 -0500

    Making the projects link look nicer

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, url_for
-
-app = Flask(__name__)
-
-@app.route("/projects")
-def projects():
-    return render_template('projects.html')
-
-app.run(debug = True)
