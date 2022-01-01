545f4f444dce0ed5b8fb12875e56fdca1c175d2a
Abby Nacional <aan16@duke.edu>
2022-01-01 16:13:12 -0500

    Trying to get projects page to work

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/projects")
-def show_projects():
+def projects():
     return render_template('projects.html')
 
 app.run(debug = True)
